c25a12ddbf09cdad5ff5cdf736c065e8460f3f2c
Caleb Pace <pacecaleb2004+Git@gmail.com>
2025-10-31 17:12:12 +1300

    feat: take winning moves

--- agents/heuristic_agent.py
+++ agents/heuristic_agent.py
@@ -1,8 +1,20 @@
 import random
 from .agent_base import AgentBase
-from game import Game
+from game import Game, COL_COUNT
 
 class HeuristicAgent(AgentBase):
     def move(self, g: Game):
-        g.try_drop_disc(random.randint(0, 6)) # TODO: Remove, for debugging
+        # Look for winning moves
+        for c in range(COL_COUNT):
+            if g.is_valid_location(c):
+                # Simulate drop
+                temp_grid = g.get_grid()
+                g.drop_disc(temp_grid, self.player_id, c)
+
+                # Check if win
+                if g.check_win(temp_grid, self.player_id):
+                    g.try_drop_disc(c) # Play winning move
+                    return
+        
+        g.try_drop_disc(random.randint(0, 6))
         return
