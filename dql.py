d3c44db9925e5fdc5306b696a93c654b88ac9e61
Caleb Pace <pacecaleb2004+Git@gmail.com>
2025-11-03 14:22:33 +1300

    feat: create deep q learning class structure

---
+++ dql.py
@@ -0,0 +1,26 @@
+import random, torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import deque
+
+class DQN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class ReplayMemory():
+    pass
+
+class Connect4DQL():
+    def __init__(self):
+        # Hyperparameters
+        pass
+
+    def train(self):
+        pass
+
+    def optimise(self):
+        pass
+
+    def test(self):
+        pass
